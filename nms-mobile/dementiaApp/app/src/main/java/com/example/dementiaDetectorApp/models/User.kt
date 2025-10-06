8bb2252e02e2f7ec252b95526ffb619b8118be30
Ben Murphy <benmurphy1706@gmail.com>
2025-10-06 07:57:14 +0100

    Deleted the User object file as its now redundant

--- nms-mobile/dementiaApp/app/src/main/java/com/example/dementiaDetectorApp/models/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dementiaDetectorApp.models
-
-data class User(var accountID: String, var fName: String, var lName:String, var phoneNum: String, var address: Address) {
-    val fullName: String get() = "$fName $lName"
-}
